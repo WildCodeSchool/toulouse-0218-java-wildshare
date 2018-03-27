f8d350708bcbf5f2b22d1e9536634a4e9be9ebb4
HugoCalas <hugo.calas31@gmail.com>
2018-03-27 15:12:10 +0200

    MainActivity vide pour merge

--- app/src/main/java/fr/wildcodeschool/wildshare/MainActivity.java
+++ app/src/main/java/fr/wildcodeschool/wildshare/MainActivity.java
@@ -13,15 +13,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = findViewById(R.id.button);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                final Intent intent = new Intent(MainActivity.this, OwnItemList.class);
-                MainActivity.this.startActivity(intent);
-            }
-        });
     }
 }
