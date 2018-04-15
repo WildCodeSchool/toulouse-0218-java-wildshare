5d645d7c19910ac8c287a04502b44e4e0a1a8586
Mathieu Grégoire <https://github.com/greg-mathieu>
2018-04-15 22:20:29 +0200

    New account + progress bar

--- app/src/main/java/fr/wildcodeschool/wildshare/MainActivity.java
+++ app/src/main/java/fr/wildcodeschool/wildshare/MainActivity.java
@@ -1,33 +1,34 @@
 package fr.wildcodeschool.wildshare;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.ActionMode;
+import android.text.TextUtils;
 import android.view.View;
-import android.widget.CheckBox;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
     public static String CACHE_USERNAME = "username";
     public static String EXTRA_LOGIN = "EXTRA_LOGIN";
 
-    private EditText editLogin;
+    private EditText editEmail;
     private EditText editPassword;
+    private EditText newEmail;
+    private EditText newPassword;
     private ImageView imageLogin;
+    Button btnNewAccount;
+    ProgressBar progressBar;
 
 
     private FirebaseAuth mAuth;
@@ -38,27 +39,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);//TODO rentrer bon layout
         mAuth = FirebaseAuth.getInstance();
+        progressBar = findViewById(R.id.progressbar);
         //initialiser les sharedPreferences
         //final SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
 
         //récupérer le username du cache s'il existe
         //String usernameCache = sharedPref.getString(CACHE_USERNAME, "");
         //editLogin.setText(usernameCache);
-
+        editEmail = findViewById(R.id.edit_email);
+        editPassword = findViewById(R.id.edit_password);
         imageLogin = findViewById(R.id.image_log);
         imageLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                editLogin = findViewById(R.id.edit_pseudo);
-                String loginValue = editLogin.getText().toString();
-                editPassword = findViewById(R.id.edit_password);
+
+                String loginValue = editEmail.getText().toString();
+
                 String passwordValue = editPassword.getText().toString();
+                progressBar.setVisibility(view.VISIBLE);
                 if (loginValue.isEmpty() || passwordValue.isEmpty()) {
+                    progressBar.setVisibility(View.GONE);
                     Toast.makeText(MainActivity.this, R.string.enter_pseudo_password, Toast.LENGTH_SHORT).show();
                 } else {
                     mAuth.signInWithEmailAndPassword(loginValue, passwordValue).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
+                            progressBar.setVisibility(View.GONE);
                             if (!task.isSuccessful()){
                                 Toast.makeText(MainActivity.this, "Incorrect email or password", Toast.LENGTH_SHORT).show();
                             }
@@ -77,6 +83,65 @@ public class MainActivity extends AppCompatActivity {
 
         });
 
+
+
+        ImageView imageSignUp = findViewById(R.id.image_signup);
+        imageSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                newEmail = findViewById(R.id.new_mail);
+                newPassword = findViewById(R.id.new_password);
+                btnNewAccount = findViewById(R.id.btnNewAccount);
+                final ImageView buttonSignIn = findViewById(R.id.image_log);
+
+                ImageView logo = findViewById(R.id.image_logo_fond);
+                newPassword.setVisibility(View.VISIBLE);
+                newEmail.setVisibility(View.VISIBLE);
+                btnNewAccount.setVisibility(View.VISIBLE);
+                editEmail.setVisibility(View.GONE);
+                editPassword.setVisibility(View.GONE);
+                buttonSignIn.setVisibility(View.GONE);
+
+                btnNewAccount.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String emailN = newEmail.getText().toString();
+                        String passwordN = newPassword.getText().toString();
+                        if (TextUtils.isEmpty(emailN) || (TextUtils.isEmpty(passwordN))){
+                            Toast.makeText(MainActivity.this, "Enter both values", Toast.LENGTH_SHORT).show();
+                        }
+                        else {
+                            mAuth.createUserWithEmailAndPassword(emailN, passwordN).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()){
+                                        Toast.makeText(MainActivity.this, "Account create", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                        }
+                    }
+                });
+
+                ImageView imageSignUp = findViewById(R.id.image_signup);
+                imageSignUp.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        newPassword.setVisibility(View.GONE);
+                        newEmail.setVisibility(View.GONE);
+                        btnNewAccount.setVisibility(View.GONE);
+                        buttonSignIn.setVisibility(View.VISIBLE);
+                        editEmail.setVisibility(View.VISIBLE);
+                        editPassword.setVisibility(View.VISIBLE);
+
+
+                    }
+                });
+            }
+        });
+
+
+
         authStateListener = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
@@ -87,33 +152,6 @@ public class MainActivity extends AppCompatActivity {
 
             }
         };
-
-        ImageView imageSignUp = findViewById(R.id.image_signup);
-        imageSignUp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                final EditText editconfirm = findViewById(R.id.edit_confirm);
-                final EditText editemail = findViewById(R.id.edit_mail);
-                final ImageView buttonSignIn = findViewById(R.id.image_log);
-
-                ImageView logo = findViewById(R.id.image_logo_fond);
-                editconfirm.setVisibility(View.VISIBLE);
-                editemail.setVisibility(View.VISIBLE);
-
-                buttonSignIn.setVisibility(View.GONE);
-
-                ImageView imageSignUp = findViewById(R.id.image_signup);
-                imageSignUp.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        editconfirm.setVisibility(View.GONE);
-                        editemail.setVisibility(View.GONE);
-
-                        buttonSignIn.setVisibility(View.VISIBLE);
-                    }
-                });
-            }
-        });
     }
 
     @Override
